3fb12fa8fbd92121446baf9a29d2875e79259cb7
RC Pelisco <rcpelisco@gmail.com>
2018-08-22 16:45:11 +0800

    updated logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,7 +3,7 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <a class="navbar-brand" href="#">
-    <img class="navbar-brand-full" src="/images/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+    <img class="navbar-brand-full" src="/images/brand/logo.png" width="89" height="25" alt="CoreUI Logo">
     <img class="navbar-brand-minimized" src="/images/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
   </a>
   <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" data-toggle="sidebar-lg-show">
@@ -22,7 +22,7 @@
   </ul> --}}
   <ul class="nav navbar-nav ml-auto">
     <li class="nav-item dropdown">
-      <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      <a class="nav-link " data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
         <img class="img-avatar" src="/images/avatars/6.jpg" alt="admin@bootstrapmaster.com">
       </a>
       <div class="dropdown-menu dropdown-menu-right">
